ab4e4d8dfa8230be52ca98aa75939b913df8c623
pratik <pratikhanmante3@gmail.com>
2025-03-31 19:13:17 +0530

    repo initialized

---
+++ src/main/java/com/pratik/restaurant/RestaurantReviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.pratik.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReviewPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReviewPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratik/restaurant/RestaurantReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratik.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
