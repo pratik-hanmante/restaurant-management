9b8ece1eafbbe72a4446bcd993c86bc0030634da
pratik-hanmante <84140896+pratik-hanmante@users.noreply.github.com>
2025-04-18 20:33:20 +0530

    Update Review.java

--- src/main/java/com/pratik/restaurant/domain/entities/Review.java
+++ src/main/java/com/pratik/restaurant/domain/entities/Review.java
@@ -8,7 +8,6 @@ import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,25 +18,24 @@ import java.util.List;
 @Builder
 public class Review {
 
-    @Field(type = FieldType.Keyword)
+    @Field(type = FieldType.Keyword) // Maps the 'id' field to a keyword field in Elasticsearch for exact matches
     private String id;
 
-    @Field(type = FieldType.Text)
+    @Field(type = FieldType.Text) // Maps the 'content' field to a text field in Elasticsearch for full-text search
     private String content;
 
-    @Field(type = FieldType.Integer)
+    @Field(type = FieldType.Integer) // Maps the 'rating' field to an integer type in Elasticsearch
     private Integer rating;
 
-    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second) // Maps the 'datePosted' field to a date field with format "date_hour_minute_second" in Elasticsearch
     private LocalDateTime datePosted;
 
-
-    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second) // Maps the 'lastEdited' field to a date field with format "date_hour_minute_second" in Elasticsearch
     private LocalDateTime lastEdited;
 
-    @Field(type = FieldType.Nested)
+    @Field(type = FieldType.Nested) // Maps the 'photos' field as a nested object in Elasticsearch
     private List<Photo> photos = new ArrayList<>();
 
-    @Field(type = FieldType.Nested)
+    @Field(type = FieldType.Nested) // Maps the 'writtenBy' field as a nested object in Elasticsearch
     private User writtenBy;
 }
