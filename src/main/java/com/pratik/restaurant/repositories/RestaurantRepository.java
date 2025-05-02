b5c478476c33695b03a6a9fd17ae39dd1b36db55
pratik <pratikhanmante3@gmail.com>
2025-05-02 23:30:22 +0530

    modified restaurantRepo

---
+++ src/main/java/com/pratik/restaurant/repositories/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package com.pratik.restaurant.repositories;
+
+import com.pratik.restaurant.domain.entities.Restaurant;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends ElasticsearchRepository<Restaurant, String> {
+        // custom queries
+}
