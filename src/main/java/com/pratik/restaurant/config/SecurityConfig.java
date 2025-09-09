477aec295676d696baf9ea50627e992e168d0a56
pratik-hanmante <84140896+pratik-hanmante@users.noreply.github.com>
2025-09-09 22:19:00 +0530

    Update SecurityConfig.java

--- src/main/java/com/pratik/restaurant/config/SecurityConfig.java
+++ src/main/java/com/pratik/restaurant/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                // Step 1: Enforce that every HTTP request must be authenticated
+             
                 .authorizeHttpRequests(auth ->
                         auth.anyRequest().authenticated()
                 )
